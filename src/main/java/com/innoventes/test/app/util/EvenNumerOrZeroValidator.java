17e082025976d0016d2cd3d5657b3ff9e7685e32
ABHISHEK BHADAURIYA <112882023+AbhishekbhadauriyaKK@users.noreply.github.com>
2026-02-18 15:58:49 +0530

    Update EvenNumerOrZeroValidator.java

--- src/main/java/com/innoventes/test/app/util/EvenNumerOrZeroValidator.java
+++ src/main/java/com/innoventes/test/app/util/EvenNumerOrZeroValidator.java
@@ -1,1 +1,7 @@
-
+public class EvenNumberOrZeroValidator implements ConstraintValidator<EvenNumberOrZero, Number>{
+  @Override
+  public boolean isValid(Number value, ConstraintValidatorContext context){
+    if(value==null return true;
+    long num=value.longValue();
+     return num==0 || num%2==0;
+  
